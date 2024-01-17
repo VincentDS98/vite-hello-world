f02bf12cec30cf3918b7c07ccadebc2a6f693ef6
VincentDS98 <disanto.vincenzo98@gmail.com>
2024-01-17 14:31:28 +0100

    Hello

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script >
+
+export default {
+  data() {
+    return {
+      
+    };
+  },
+  
+};
+</script>
+
+<template>
+  <div>
+    
+    <h1>
+      Hello World
+    </h1>
+    
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,24 @@
+<script >
+export default {
+  data() {
+    return {
+     title: 'Ciao ragazzi'
+    };
+  },
+  
+};
+</script>
+
+<template>
+  <div>
+        <header>
+            <h1>
+                {{ title }}
+            </h1>
+        </header>
+    </div>  
+</template>
+
+<style scoped>
+
+</style>
